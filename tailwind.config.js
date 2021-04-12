c742205eaad972b4e549a5ee9c95fed093cf450f
Miroslav Jonas <missing.manual@gmail.com>
2021-04-12 19:17:36 +0200

    feat: fix the package

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
